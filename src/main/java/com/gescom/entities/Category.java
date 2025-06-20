cc4a1e21369966eaf0c1a50ecfa2d5cbcde6e077
ghost_designer <congotech17@gmail.com>
2025-06-20 16:26:22 +0200

    First commit

--- src/main/java/com/gescom/entities/Category.java
+++ src/main/java/com/gescom/entities/Category.java
@@ -1,4 +1,19 @@
 package com.gescom.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 60)
+    private String designation;
+    private String description;
 }
---
+++ src/main/java/com/gescom/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.gescom.repositories;
+
+import com.gescom.entities.Category;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
